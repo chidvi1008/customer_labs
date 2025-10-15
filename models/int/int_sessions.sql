4db9124a158ca6b13bd6d18d290bd8da4db88c82
Chidu Napa <chidunapa143@gmail.com>
2025-10-15 21:27:52 +0800

    int_session.sql change

--- models/int/int_sessions.sql
+++ models/int/int_sessions.sql
@@ -1,10 +1,20 @@
 {{ config(materialized='table') }}
 
-select
+SELECT
     session_id,
     user_pseudo_id,
-    min(event_ts) as session_start,
-    max(event_ts) as session_end
-from {{ ref('stg_events') }}
-group by 1,2
+    MIN(event_ts) AS session_start,
+    MAX(event_ts) AS session_end,
+    TIMESTAMPDIFF(MINUTE, MIN(event_ts), MAX(event_ts)) AS session_duration_min,
+    COUNT(*) AS events_in_session,
+    ARRAY_AGG(event_name ORDER BY event_ts)[1] AS first_event,
+    ARRAY_AGG(event_name ORDER BY event_ts DESC)[1] AS last_event,
+    CASE 
+        WHEN TIMESTAMPDIFF(MINUTE, MIN(event_ts), MAX(event_ts)) > 60 THEN 1
+        ELSE 0
+    END AS is_long_session
+FROM {{ ref('stg_events') }}
+GROUP BY 1,2
+HAVING COUNT(*) > 1
+ORDER BY session_start;
 
