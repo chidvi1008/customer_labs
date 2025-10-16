a3057d5694c1d645dcddfaef56a8b0b1543774d3
chidvi1008 <89209244+chidvi1008@users.noreply.github.com>
2025-10-16 15:08:12 +0800

    Enhance dashboard with new data sections and titles
    Updated subheader titles and added new data sections for session and live panel events.

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,16 @@ import pandas as pd
 
 con = duckdb.connect("data/customer_labs.duckdb")
 
-st.title("📈 Real-time Attribution Dashboard")
+st.title("📈 Real-Time Attribution Dashboard")
 
-st.subheader("Attribution Summary")
-df = con.execute("select * from mart_attribution_agg").df()
-st.dataframe(df)
+st.subheader("🧭 Attribution Summary")
+df = con.execute("SELECT * FROM mart_attribution_agg").df()
+st.dataframe(df, use_container_width=True)
+
+st.subheader("📊 Session Data")
+df = con.execute("SELECT * FROM int_events").df()
+st.dataframe(df, use_container_width=True)
+
+st.subheader("Live panel events")
+df = con.execute("SELECT * FROM stg_events").df()
+st.dataframe(df, use_container_width=True)
