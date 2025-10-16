8d9bf59f1446bc95b7fdf9404a077ed9bf29b95f
chidvi1008 <89209244+chidvi1008@users.noreply.github.com>
2025-10-16 15:06:53 +0800

    Refactor SQL to include attribution union for users count

--- models/mart/mart_attribution_agg.sql
+++ models/mart/mart_attribution_agg.sql
@@ -1,17 +1,30 @@
 {{ config(materialized='table') }}
 
-SELECT
-    'First Click' AS model_type,
-    first_source AS source,
-    COUNT(*) AS users
-FROM {{ ref('int_events') }}
-GROUP BY 1,2
+WITH attribution_union AS (
+    SELECT 
+        'First Click' AS model_type,
+        first_source AS source,
+        first_medium AS medium,
+        CAST(first_click_ts AS DATE) AS event_date
+    FROM {{ ref('int_events') }}
 
-UNION ALL
+    UNION ALL
+
+    SELECT 
+        'Last Click' AS model_type,
+        last_source AS source,
+        last_medium AS medium,
+        CAST(last_click_ts AS DATE) AS event_date
+    FROM {{ ref('int_events') }}
+)
 
 SELECT
-    'Last Click' AS model_type,
-    last_source AS source,
-    COUNT(*) AS users
-FROM {{ ref('int_events') }}
-GROUP BY 1,2
+    model_type,
+    source,
+    medium,
+    event_date,
+    COUNT(*) AS user_count
+FROM attribution_union
+WHERE event_date >= current_date - INTERVAL 14 DAY
+GROUP BY 1,2,3,4
+ORDER BY event_date, model_type, source
